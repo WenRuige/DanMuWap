4f3c15130974e3b357ca1f8036ece2378be8f3aa
gewenrui <941137860@qq.com>
2017-03-01 22:35:49 +0800

    chekc

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -10,6 +10,7 @@ namespace App\Http\Controllers\Index;
 use App\Constant;
 use App\Events\ExampleEvent;
 use App\Http\Controllers\Controller;
+use core\User\UserService;
 use Illuminate\Http\Request;
 use App\Logic\Index\IndexLogic;
 use Cache;
@@ -22,18 +23,21 @@ class IndexController extends Controller
     //首页展示
     public function index()
     {
-        IndexService::getInstance()->index();
-        $pd = Container::getMysql();
-        $data = $pd->from('users')->fetch();
-        //启用验证系统
-        $res = [];
-        $info = IndexLogic::getInstance()->show();
-        if ($info['code'] == Constant::SUCCESS) {
-            $res = $info['data'];
-        } else {
-            //如果返回不是成功的话,跳转到错误界面
-            echo 'error';
-        }
-        return view('Index.index', ['data' => $res]);
+        $videoInfo = IndexService::getInstance()->index();
+
+                UserService::getInstance()->getUserInformation();
+
+//        $pd = Container::getMysql();
+//        $data = $pd->from('users')->fetch();
+//        //启用验证系统
+//        $res = [];
+//        $info = IndexLogic::getInstance()->show();
+//        if ($info['code'] == Constant::SUCCESS) {
+//            $res = $info['data'];
+//        } else {
+//            //如果返回不是成功的话,跳转到错误界面
+//            echo 'error';
+//        }
+//        return view('Index.index', ['data' => $res]);
     }
 }
